437f4ed33f0a3c0b8ea1e5c10633631fcc37cb17
Andrei Ujica <andrei@vipra.tech>
2024-03-17 17:42:07 +0100

    fix: install pytorch and set trust remote code flag to true in order to load the big_patent dataset

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
 def get_dataset_sample(category="g", num_samples=10):
-    dataset = load_dataset("big_patent", category, split='test')
+    dataset = load_dataset("big_patent", category, split='test', trust_remote_code=True)
     samples = dataset.select(range(num_samples))
     return [{"text": sample["description"], "summary": sample["abstract"]} for sample in samples]
