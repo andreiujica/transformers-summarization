5478e6b3355c5ce8b26a6aa8cb9c13db91d839c0
Andrei Ujica <andrei@vipra.tech>
2024-03-18 22:27:01 +0100

    feat: install sentencepiece for tokenizers

--- utils/data.py
+++ utils/data.py
@@ -1,6 +1,9 @@
 from datasets import load_dataset
 
 def get_dataset_sample(category="g", num_samples=10):
+    """
+    This function loads a small set of samples from the BIGPATENT dataset for demonstration and initial setup.
+    """
     dataset = load_dataset("big_patent", category, split='test', trust_remote_code=True)
     samples = dataset.select(range(num_samples))
     return [{"text": sample["description"], "summary": sample["abstract"]} for sample in samples]
