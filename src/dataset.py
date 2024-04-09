68ab01676fbd92f4c7a711aa937b5c8d8492a58d
Andrei Ujica <andrei@vipra.tech>
2024-04-09 14:37:41 +0200

    chore(yaml): change yaml call to see structure

--- src/dataset.py
+++ src/dataset.py
@@ -12,7 +12,7 @@ from datasets import load_dataset
 DATA_CONFIG_FILE = 'config/dataset.yaml'
 
 with open(DATA_CONFIG_FILE, 'r') as file:
-    data_config = yaml.safe_load(file)['dataset'][0]
+    data_config = yaml.safe_load(file)['dataset']
 
 def get_dataset(only_samples=True):
     """
