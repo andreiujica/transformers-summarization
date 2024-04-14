7cf547db40f199b9ed0ea00d3d71330b1b76a8a2
Andrei Ujica <andrei@vipra.tech>
2024-04-14 14:11:25 +0200

    fix: revert back to zero-shot inference for the evaluator

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,61 +1,35 @@
+"""
+This file contains utility functions for running the summarizationEvaluator.
+It provides functions to:
+- Compute the ROUGE, BLEU, and METEOR scores
+"""
+
 import yaml
-from evaluate import load
-from transformers import AutoTokenizer, pipeline
-import concurrent.futures
+from evaluate import evaluator, combine
 
 DATA_CONFIG_FILE = 'config/dataset.yaml'
-CONTEXT_LENGTH = 512
 
 with open(DATA_CONFIG_FILE, 'r') as file:
     data_config = yaml.safe_load(file)['dataset']
 
-metrics = {'rouge': load('rouge'), 'sacrebleu': load('sacrebleu'), 'meteor': load('meteor')}
-
-def compute_average_metrics(metrics):
-    average_scores = {metric_name: {key: sum(metric[metric_name][key] for metric in metrics) / len(metrics)
-                                    for key in metrics[0][metric_name]}
-                      for metric_name in ['rouge', 'bleu', 'meteor']}
-    return average_scores
-
-def split_into_chunks(tokenizer, text, overlap=50):
-    tokenized_text = tokenizer.encode(text)
-    size = CONTEXT_LENGTH - overlap
-    chunks = [tokenized_text[i:i + size] for i in range(0, len(tokenized_text), size)]
-    return [tokenizer.decode(chunk) for chunk in chunks]
-
-def summarize_and_evaluate(model, tokenizer, example, few_shot_prefix=""):
-    input_text = few_shot_prefix + example[data_config['input_column']]
-    reference = example[data_config['summary_column']]
-    chunks = split_into_chunks(tokenizer, input_text)
-    summaries = [model(chunk)[0]['summary_text'] for chunk in chunks]
-    combined_summary = " ".join(summaries)
-    return {
-        "rouge": metrics['rouge'].compute(predictions=[combined_summary], references=[reference]),
-        "bleu": {"score": metrics['sacrebleu'].compute(predictions=[combined_summary], references=[reference])["score"]},
-        "meteor": metrics['meteor'].compute(predictions=[combined_summary], references=[reference])
-    }
-
 def run_evaluation_suite(model_name, dataset):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = pipeline("summarization", model=model_name)
-    few_shot_prefix = " ".join(["Text: " + item[data_config['input_column']] + " Summary: " + item[data_config['summary_column']]
-                                for item in dataset.select(range(2))]) + " "
+    """
+    Runs the summarizationEvaluator to compute the ROUGE, BLEU, and METEOR scores
+    for a single model on an entire dataset.
 
-    zero_shot_metrics, few_shot_metrics = [], []
+    Params:
+    - model_name: The name of the model to evaluate
+    - dataset: The dataset object
+    """
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        futures_zero_shot = [executor.submit(summarize_and_evaluate, model, tokenizer, example) for example in dataset]
-        futures_few_shot = [executor.submit(summarize_and_evaluate, model, tokenizer, example, few_shot_prefix) for example in dataset]
+    summarization_evaluator = evaluator('summarization')
 
-        zero_shot_metrics = [future.result() for future in futures_zero_shot]
-        few_shot_metrics = [future.result() for future in futures_few_shot]
+    return summarization_evaluator.compute(
+        model_or_pipeline=model_name,
+        data=dataset,
+        metric=combine(['rouge', 'sacrebleu', 'meteor']),
+        input_column=data_config['input_column'],
+        label_column=data_config['summary_column'],
+        strategy='bootstrap',
+    )
 
-    zero_shot_results = compute_average_metrics(zero_shot_metrics)
-    few_shot_results = compute_average_metrics(few_shot_metrics)
-
-    results_json = {
-        "zero-shot": zero_shot_results,
-        "few-shot": few_shot_results
-    }
-
-    return results_json
