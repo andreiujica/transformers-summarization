3b3f274bea70512fd2d1654f508191318de2ae6f
Andrei Ujica <andrei@vipra.tech>
2024-05-14 16:54:36 +0200

    feat: add parallel processing

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from evaluate import load
 from tqdm.auto import tqdm
 from src.summarize import load_model_and_tokenizer, summarize_via_tokenbatches
 import logging
+import concurrent.futures
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +23,12 @@ MODEL_CONFIG_FILE = 'config/models.yaml'
 with open(MODEL_CONFIG_FILE, 'r') as file:
     models_config = yaml.safe_load(file)['models']
 
+def process_item(item, model, tokenizer, max_input_length):
+    input_text = item['description'] 
+    reference = item['abstract'] 
+    summary = summarize_via_tokenbatches(input_text, model, tokenizer, batch_length=max_input_length)
+    return summary, reference
+
 # Evaluate a single model
 def evaluate_model(model_name):
     for model in models_config:
@@ -33,18 +40,18 @@ def evaluate_model(model_name):
 
     dataset = load_dataset(data_config['name'], data_config['category'], split='test', trust_remote_code=True, streaming=True)
     logger.info(f"Loaded dataset {data_config['name']}")
-    
+
     rouge = load('rouge')
     bleu = load('sacrebleu')
     meteor = load('meteor')
 
     predictions, references = [], []
-    for item in tqdm(dataset, desc=f"Evaluating {model_name}"):
-        input_text = item[data_config['input_column']]
-        reference = item[data_config['summary_column']]
-        summary = summarize_via_tokenbatches(input_text, model, tokenizer, batch_length=model_info['max_input_length'])
-        predictions.append(summary)
-        references.append(reference)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_item, item, model, tokenizer, model_info["max_input_length"]) for item in dataset]
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc=f"Evaluating {model_name}"):
+            summary, reference = future.result()
+            predictions.append(summary)
+            references.append(reference)
 
     # Compute metrics
     rouge_scores = rouge.compute(predictions=predictions, references=references)
