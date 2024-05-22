fb18d0251b1fbab16d6ea0ec5cf3a1d3c4d5c9df
Andrei Ujica <andrei@vipra.tech>
2024-05-22 19:48:58 +0200

    fix: overflow error when decoding predicitons

--- app.py
+++ app.py
@@ -50,7 +50,10 @@ rouge = load("rouge")
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
+    
+    predictions = np.where(predictions != -100, predictions, tokenizer.pad_token_id)
     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
